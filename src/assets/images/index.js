89780cd637b59524a94eab006cfe02173fe543b2
Surinder Singh <surindersingh@codebrewinnovations.com>
2024-12-26 17:49:35 +0530

    testing commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,7 @@
+export const imagesPath = {
+    icPriceTag: require('./icCart.png'),
+    icLike: require('./icCart.png'),
+    icLiked:require('./icCart.png'),
+    icCart: require('./icCart.png'),
+    icAddToCart: require('./icAddToCart.png')
+}
